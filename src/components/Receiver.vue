8c6bf4467fe5f485bf49cc6df71423f0e2075923
luda412 <rudaroy@naver.com>
2026-02-25 17:43:14 +0900

    [Feat] 이벤트 수신

---
+++ src/components/Receiver.vue
@@ -0,0 +1,24 @@
+<script setup>
+import emitter from '@/eventBus';
+import { onMounted, ref } from 'vue';
+
+const message = ref('')
+
+function handleMessage(data){
+    message.value = data;
+}
+
+// 컴포넌트가 마운트될 때 이벤트 등록
+onMounted(() => {
+    emitter.on('send-message', handleMessage)
+})
+
+// 메모리 관리를 위한 off
+onMounted(()=> {
+    emitter.off('send-message', handleMessage)
+})
+</script>
+
+<template>
+    <p>받은 메시지: {{ message }}</p>
+</template>
