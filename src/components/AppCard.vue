a5db8cfd6b586a43897eb251214eb6800b2e7590
luda412 <rudaroy@naver.com>
2026-02-25 10:03:56 +0900

    [Feat] - Props

---
+++ src/components/AppCard.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>{{ title }}</p>
+    <p>{{ contents }}</p>
+</template>
+
+<script setup>
+const props = defineProps({
+    title: String,
+    contents: String,
+})
+//script에서 쓰고 싶으면 props.title / props.contents로 접근한다.
+//예, console.log(props.title)
+</script>
+<!-- 자식 컴포넌트인 AppCard에서 props를 선언하여 전달 받았다. -->
---
+++ src/views/TheView.vue
@@ -0,0 +1,9 @@
+<template>
+    <AppCard title="제목1" contents="내용1"/>
+</template>
+
+<script setup>
+import AppCard from '@/components/AppCard.vue';
+</script>
+
+<!-- 부모 컴포넌트 TheView에서 자식 컴포넌트인 AppCard에 title, content 전달 -->
