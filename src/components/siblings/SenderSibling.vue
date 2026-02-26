c62d8d8e23ef615455cdde9cde4d461f212e2a75
luda412 <rudaroy@naver.com>
2026-02-26 12:35:39 +0900

    [Feat] Sibling component emitter

---
+++ src/components/siblings/SenderSibling.vue
@@ -0,0 +1,14 @@
+<script setup>
+import emitter from '@/shared/eventBus';
+
+const send =()=> {
+    emitter.emit('sibling:message', {
+        text: 'Hello from SenderSibling', 
+        at: Date.now()
+    })
+}
+</script>
+
+<template>
+    <button @click="send">형제에게 메시지 보내기</button>
+</template>
