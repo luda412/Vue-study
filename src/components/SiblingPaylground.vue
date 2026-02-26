658ee395a2876f0c174bac75a6a3559c9177d454
luda412 <rudaroy@naver.com>
2026-02-26 12:33:46 +0900

    [Feat] 형제 컴포넌트 통신 view

---
+++ src/components/SiblingPaylground.vue
@@ -0,0 +1,12 @@
+<script setup>
+import ReciverSiblings from './siblings/ReciverSiblings.vue';
+import SenderSibling from './siblings/SenderSibling.vue';
+
+
+</script>
+
+<template>
+    <h2>Sibling Playground</h2>
+    <SenderSibling />
+    <ReciverSiblings />
+</template>
