6b1079a8431dfdf4174a7e7381db1a7f3f432351
luda412 <rudaroy@naver.com>
2026-02-26 16:12:27 +0900

    [Feat] footer 구현

---
+++ src/layouts/FooterBar.vue
@@ -0,0 +1,8 @@
+<script setup>
+
+</script>
+<template>
+    <footer style="padding: 14px 20px; border-top: 1px solid #ddd">
+        <small>© Demo App - Vue 3 Router Named Views</small>
+    </footer>
+</template>
