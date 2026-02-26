439b1ec5385f844848a0b5a9d26d9dc80deefef4
luda412 <rudaroy@naver.com>
2026-02-26 12:34:26 +0900

    [Feat] DeepChild component emitter

---
+++ src/components/ancestor/DeepChild.vue
@@ -0,0 +1,14 @@
+<script setup>
+import emitter from '@/shared/eventBus';
+
+const notifyAncestor = () => {
+    emitter.emit('ancestor:notify', {
+        from: 'DeepChild',
+        text: '조상에게 데이터 보내기',
+        at: Date.now(),
+    })
+}
+</script>
+<template>
+    <button @click="notifyAncestor">조상에게 보내기</button>
+</template>
