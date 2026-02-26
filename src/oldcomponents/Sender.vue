c91fed02ec933a7702a1048dff9b70323a15656b
luda412 <rudaroy@naver.com>
2026-02-26 09:26:28 +0900

    [Refector] components foilder move to old components

---
+++ src/oldcomponents/Sender.vue
@@ -0,0 +1,14 @@
+<script setup>
+import emitter from '@/eventBus';
+
+
+function sendMessage(){
+    emitter.emit('send-message', "Hello from Sender")
+}
+</script>
+
+<template>
+    <button @click="sendMessage">
+        형제에게 메시지 보내기
+    </button>
+</template>
