553f99e76ebd2c4391d4a26af7fc9e81a84e9643
luda412 <rudaroy@naver.com>
2026-02-26 16:12:14 +0900

    [Refector] AppVue에서 사용하던 컴포넌트와 함수를 Header와 footer로 이관하여 컴포넌트를 정의해서 사용

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,18 @@
 <script setup>
-import { computed } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
+import { RouterView } from 'vue-router';
 
-const route = useRoute()
-const router = useRouter()
 
-const isLoggedIn = computed(() => localStorage.getItem('demo.loggedIn') === 'ture')
-
-const toggleLogin = async () => {
-  localStorage.setItem('demo.loggedIn', String(!isLoggedIn.value))
-  await router.replace(route.fullPath)
-}
 </script>
 
 <template>
   <div style="max-width: 920px; margin: 0 auto; padding: 20px">
-    <header style="display: flex; justify-content: space-between; align-items:center;">
-      <nav style="display: flex; gap:12px; align-items: center;">
-        <!-- RouterLink를 통한 경로 이동 -->
-        <RouterLink to="/" active-class="active">Home</RouterLink>
-        <RouterLink to="/event-bus" active-class="active">EventBus</RouterLink>
-        <RouterLink to="/the" active-class="active">TheView</RouterLink>
-        <RouterLink to="/users" active-class="active">Users(카드)</RouterLink>
-      </nav>
-
-      <button @click="toggleLogin">
-        {{ isLoggedIn ? 'Logout(데모)' : 'Login(데모)' }}
-      </button>
-    </header>
-
-    <hr style="margin: 16px 0;" />
-    
-    <!-- router view -->
-     <RouterView v-slot="{Component}">
-      <KeepAlive>
-        <component :is="Component" />
-      </KeepAlive>
-     </RouterView>
+    <RouterView name="header" />
+    <hr sytle="margin: 0;" />
+    <main style="padding: 20px;" >
+      <RouterView />
+    </main>
+    <hr sytle="margin: 0;" />
+    <RouterView name="footer" />
   </div>
 </template>
 
