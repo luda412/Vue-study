8a4f5defcff0209b8131d4559b96a973d3338fe4
luda412 <rudaroy@naver.com>
2026-02-26 13:31:47 +0900

    [Feat] router setting

--- src/main.js
+++ src/main.js
@@ -1,13 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-const app = createApp(App)
-
-app.component('MyComponent', {
-    template: '<div>전역 컴포넌트가 등록되었습니다.</div>'
-})
-
-app.mount('#app')
 
 createApp(App)
     .use(router)
