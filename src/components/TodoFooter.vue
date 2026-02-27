d607791ec132fc1f49c1b49c2b5fdc397116036b
luda412 <rudaroy@naver.com>
2026-02-27 16:45:03 +0900

    [Feat] 할 일 모두 삭제 feture footer

---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,26 @@
+<!-- 할 일 모두 삭제 -->
+<script setup>
+const clearTodo =()=>{
+    localStorage.clear();
+}
+</script>
+
+<template>
+    <div class="clearAllContainer">
+        <span class="clearAllBtn" @click="clearTodo">Clear All</span>
+    </div>
+</template>
+<style scoped>
+    .clearAllContainer{
+        width: 8.5rem;
+        height: 50px;
+        line-height: 50px;
+        background-color: white;
+        border-radius: 5px;
+        margin: 0 auto;
+    }
+    .clearAllBtn {
+        color: #e20303;
+        display: black;
+    }
+</style>
