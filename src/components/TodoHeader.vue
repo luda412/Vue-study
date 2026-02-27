bbb3ed9d577b59a817bcef3606096366f4bb0987
luda412 <rudaroy@naver.com>
2026-02-27 16:45:31 +0900

    [Feat] 애플리케이션 이름 표시 feature header

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,18 @@
+<!-- 애플리케이션 이름 표시 -->
+<script setup>
+
+</script>
+
+<template>
+    <header>
+        TODO it!
+    </header>
+</template>
+
+<style scoped>
+h1{
+    color: #2F3B52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+}
+</style>
