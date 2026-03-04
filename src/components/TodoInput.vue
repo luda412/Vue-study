86979148d646bcfb64a7dc7e2102905091eda21e
luda412 <rudaroy@naver.com>
2026-03-04 13:34:09 +0900

    [Feat] 경고 모달 띄우기

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -2,18 +2,28 @@
 <script setup>
 import emitter from '@/shared/eventBus';
 import { ref } from 'vue';
+import Modal from './common/modal.vue';
 
 const item = ref('')
+const showModal = ref(false);
 
 const addTodo =() =>{
     const res = item.value.trim();
-    if(!res) return;
+    if(!res){
+        showModal.value = true
+        return
+    }
     emitter.emit('todo:add', res)
     clearInput();
 }
 const clearInput =()=>{
     item.value = '';
 }
+
+const closeModal =()=>{
+    showModal.value = false;
+}
+
 </script>
 
 <template>
@@ -22,6 +32,17 @@ const clearInput =()=>{
         <span class="addContainer" @click="addTodo">
             <i class="addBtn fas fa-plus" aria-hidden="true"></i>
         </span>
+        <Modal v-if="showModal" @close="closeModal">
+            <template #header>
+                <h3>경고</h3>
+            </template>
+            <template #body>
+                <p>할 일을 입력하세요.</p>
+            </template>
+            <template #footer>
+                <button class="modal-default-button" @click="closeModal">OK</button>
+            </template>
+        </Modal>
     </div>
 </template>
 
